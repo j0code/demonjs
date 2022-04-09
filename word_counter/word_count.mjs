f907f132a5dfdecf9bc2d08fe964e3fa1a42cbdd
j0code <jonaslp759@gmail.com>
2022-04-09 23:28:15 +0200

    word_counter update - global total word count - fixing save data loading for users - add digits to "forbidden" symbols - allowing ', but disallowing ` now - bots no longer count towards global word counts - add /wordcount as replacement for "word count" +improved

--- word_counter/word_count.mjs
+++ word_counter/word_count.mjs
@@ -6,6 +6,7 @@ var saveData = await save.load("word_count", true) || await save.load("backup/wo
 
 let words
 let users
+let total
 
 export default class WordCount {
 
@@ -13,13 +14,15 @@ export default class WordCount {
 		save.write("backup/word_count", saveData)
 
 		words      = MagicMap.fromObject(saveData.words)
-		users      = MagicMap.fromObject(saveData.users)
+		users      = MagicMap.fromObject(saveData.users, {fromObject: o => {
+			return { total: o.total, words: MagicMap.fromObject(o.words)}
+		}})
+		total      = saveData.total || 0
 
 		//console.log("words:", ...words.conarr)
 		//console.log("users:", ...users.conarr)
 
 		client.on("messageCreate", msg => {
-			if(msg.author.id == client.user.id) return
 
 			let wordlist  = msg.cleanContent.split(/\s/g)
 			let user = users.get(msg.author.id) || {total: 0, words: new MagicMap()}
@@ -28,32 +31,57 @@ export default class WordCount {
 
 			for(let word of wordlist) {
 				word = word.toLowerCase()
-				word = word.replaceAll(/[!"§$%&/()=?'`^°{[\]}+*~#'<>|,;\.:-_]/g, "")
+				word = word.replaceAll(/[!"§$%&/()=?`´^°{[\]}+*~#<>|,;\.:-_1234567890]/g, "")
 				if(word.length == 0) continue
-				let globalcount = words.get(word) || 0
-				globalcount++
-				words.set(word, globalcount)
-				let usercount = user.words.get() || 0
+				if(!msg.author.bot) {
+					let globalcount = words.get(word) || 0
+					globalcount++
+					words.set(word, globalcount)
+					total++
+				}
+				let usercount = user.words.get(word) || 0
 				usercount++
 				user.words.set(word, usercount)
 			}
 
-			if(msg.content == "word count") {
-				let o = words.toJSON()
-				let arr = Array.from(Object.entries(o))
-				arr.sort((a,b) => b[1] - a[1])
-				let s = []
-				for(let i = 0; i < arr.length && i < 10; i++) {
-					const emojis = ["🥇","🥈","🥉"]
-					s.push(`${emojis[i] || "▫️"} ${arr[i][0]}: ${arr[i][1]}`)
-				}
-				if(s.length > 0) {
-					msg.reply(s.join("\n"))
-					.then(msg => msg.react("❌"))
-				}
-			}
+			users.set(msg.author.id, user)
 
 			writeSave()
+		})
+
+		client.on("interactionCreate", i => {
+			if(i.type == "APPLICATION_COMMAND" && i.commandName == "wordcount") {
+				let user = i.options.getUser("user")
+				let o
+				let s = []
+				let totalcount
+
+				if(user) {
+					if(!users.has(user.id)) return i.reply({content: "User did not chat yet :/", ephemeral: true, fetchReply: false})
+					let usercount = users.get(user.id)
+					o = usercount.words.toJSON()
+					totalcount = usercount.total
+				} else {
+					o = words.toJSON()
+					totalcount = total
+				}
+
+				let arr = Array.from(Object.entries(o))
+				arr.sort((a,b) => b[1] - a[1])
+				for(let i = 0; i < arr.length && i < 10; i++) {
+					const emojis = ["🥇","🥈","🥉"]
+					s.push(`${emojis[i] || "▫️"} \`${arr[i][0]}\`: ${arr[i][1]} *(${percent(arr[i][1], totalcount)}%)*`)
+				}
+
+				if(!s) i.reply({content: "Error: empty list", ephemeral: false, fetchReply: true})
+				.then(msg => msg.react("❌"))
+				else i.reply({content: `**TOTAL: ${total}**${user ? ` *(${percent(totalcount, total)}% of global)*` : ""}\n${s.join("\n")}`, ephemeral: false, fetchReply: true})
+				.then(msg => msg.react("❌"))
+
+				function percent(a, b) {
+					return Math.floor(a / b * 1000) / 10
+				}
+			}
 		})
 
 	}
@@ -61,5 +89,5 @@ export default class WordCount {
 }
 
 function writeSave() {
-	save.write("word_count", {words: words, users: users})
+	save.write("word_count", {words, users, total})
 }

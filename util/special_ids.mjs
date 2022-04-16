ca9681cf24744be86c3e49c005d347ab231c3817
j0code <jonaslp759@gmail.com>
2022-04-16 23:42:47 +0200

    how stupid can you be? me: yes.

--- util/special_ids.mjs
+++ util/special_ids.mjs
@@ -2,10 +2,10 @@ import fs from "fs/promises"
 
 let data
 try {
-	data = await fs.readFile("./special_user_ids.json", {encoding: "utf8"})
+	data = await fs.readFile("./special_ids.json", {encoding: "utf8"})
 	data = await JSON.parse(data)
 } catch(e) {
-	console.error(`Error loading special_user_ids:`, e)
+	console.error(`Error loading special_ids:`, e)
 	data = {}
 }
 
